0e2cb1bc095233b434afbba3281cabc7c2250f22
Muhammad Anas <anas.yousuf.31@gmail.com>
2025-04-18 17:14:33 +0500

    final changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,8 +19,9 @@ class MyApp extends StatelessWidget {
           email: "Anas@example.com",
           profileImageUrl: "https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg",
           headerColor: Colors.yellow,
-          
+          textColor: Colors.black, // ✅ makes it readable on yellow
         ),
+
 
 
         body: const Center(child: Text("Swipe from the left!")),
